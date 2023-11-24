d6ce5c4148ac68eb7d3002d20466bdba1a33ff6e
Khyati <Khyatik15@gmail.com>
2023-11-24 15:07:02 +0530

    Dashboard init

---
+++ Dashboard/script.js
@@ -0,0 +1,35 @@
+// Sidebar toggle
+const sidebarToggle=document.querySeelctor("#sidebar-toggle");
+sidebarToggle.addEventListener("click",function(){
+    document.querySelector("#sidebar").classList.toggle("collapsed");
+});
+
+
+// theme toggle
+
+document.querySelector(".theme-toggle").addEventListener("click", () => {
+    toggleLocalStorage();
+    toggleRootClass();
+  });
+  
+  function toggleRootClass() {
+    const current = document.documentElement.getAttribute("data-bs-theme");
+    const inverted = current == "dark" ? "light" : "dark";
+    document.documentElement.setAttribute("data-bs-theme", inverted);
+  }
+  
+  function toggleLocalStorage() {
+    if (isLight()) {
+      localStorage.removeItem("light");
+    } else {
+      localStorage.setItem("light", "set");
+    }
+  }
+  
+  function isLight() {
+    return localStorage.getItem("light");
+  }
+  
+  if (isLight()) {
+    toggleRootClass();
+  }
