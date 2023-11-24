e4fee06b0495e5a4fcfd874288fb26b78ad605ac
Shubhi <shubhi.paul2021@vitstudent.ac.in>
2023-11-24 22:13:56 +0530

    feat: add images to the project

--- index.js
+++ index.js
@@ -3,31 +3,58 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 
-const uploadFile = fs.readFileSync(path.join(__dirname, 'frontend', 'scheduling', 'scheduling.html'), 'utf-8');
-const uploadFileJs = fs.readFileSync(path.join(__dirname, 'frontend', 'scheduling', 'script.js'), 'utf-8');
-const combinedContent = `${uploadFile}\n<script>${uploadFileJs}</script>`;
+// upload screen
+const uploadFile = fs.readFileSync(path.join(__dirname, 'screens', 'scheduling', 'scheduling.html'), 'utf-8');
+const uploadFileJs = fs.readFileSync(path.join(__dirname, 'screens', 'scheduling', 'script.js'), 'utf-8');
+const uploadCombinedContent = `${uploadFile}\n<script>${uploadFileJs}</script>`;
+
+// dashboard screenn
+const dashboard = fs.readFileSync(path.join(__dirname, 'screens', 'dashboard', 'index.html'), 'utf-8');
+const dashboardJS = fs.readFileSync(path.join(__dirname, 'screens', 'dashboard', 'script.js'), 'utf-8');
+const dashboardCSS = fs.readFileSync(path.join(__dirname, 'screens', 'dashboard', 'style.css'), 'utf-8');
+const dashboardCombinedContent = `${dashboard}\n<script>${dashboardJS}</script><style>${dashboardCSS}</style>`;
 
 app.use(express.json());
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 // Serve landing page
 app.get('/', (req, res) => {
   console.log('Request received');
   res.status(200).send('<h2>Welcome to our NodeJS server</h2>');
 });
 
-// Serve file upload page
-app.get('/schedule', (req, res) => {
+// appointment page
+app.get('/appointment', (req, res) => {
   console.log('Request received');
-  res.status(200).send(combinedContent);
+  res.status(200).send(uploadCombinedContent);
 });
 
-// Handle POST request to /schedule
-app.post('/schedule', (req, res) => {
+// Schedule page
+app.get('/dashboard', (req, res) => {
+  console.log('Request received');
+  res.status(200).send(dashboardCombinedContent);
+});
+
+// Handle POST request to booking
+app.post('/booking', (req, res) => {
   console.log("new request :)");
   console.log(req.body);
   // Handle POST data as needed
   res.status(200).send('Received the form data');
 });
+
+// Serve file upload page
+app.get('/files', (req, res) => {
+  console.log('Request received');
+  res.status(200).send(uploadCombinedContent);
+});
+
+// User profile page
+app.get('/user', (req, res) => {
+  console.log('Request received');
+  res.status(200).send(combinedContent);
+});
+
+
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
